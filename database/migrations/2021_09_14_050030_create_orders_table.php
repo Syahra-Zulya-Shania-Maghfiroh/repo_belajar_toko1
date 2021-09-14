24380b0e077f6978aa5074d0e6522c2a2f7926cc
Syahra-Zulya-Shania-Maghfiroh <syahrazulyashaniamaghfiroh@gmail.com>
2021-09-14 12:32:57 +0700

    db_toko

---
+++ database/migrations/2021_09_14_050030_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id_orders');
+            $table->unsignedBigInteger('id_customers');
+
+            $table->foreign('id_customers')->references('id_customers')->on('customers');
+
+            $table->unsignedBigInteger('id_petugas');
+
+            $table->foreign('id_petugas')->references('id_petugas')->on('petugas');
+            
+            $table->date('tgl_transaksi');
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
